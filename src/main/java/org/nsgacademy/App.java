52a976fcc88a9b7b3a9a3c9e06e33eb17d140be9
sagrawal <suresh.rp.agrawal@gmail.com>
2025-05-15 16:25:59 +0530

    Data retrieved successfully.

---
+++ src/main/java/org/nsgacademy/App.java
@@ -0,0 +1,31 @@
+package org.nsgacademy;
+
+import java.sql.*;
+
+import static java.lang.System.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        try{
+//            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost/nsgacademy", "root", "");
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("select * from celebrity");
+            while(rs.next()){
+                out.println(rs.getString(2));
+
+            }
+
+        }catch(SQLException sql){
+            out.println(sql);
+        } catch(Exception e){
+            out.println(e);
+        }
+    }
+}
